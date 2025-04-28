a9e8dd57e5d8d95a04132c5fe18344d6a4d66714
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-04-28 15:44:16 +0200

    profiles list command

--- src/pyborg/pyborg.py
+++ src/pyborg/pyborg.py
@@ -18,10 +18,18 @@ from pathlib import Path
 # external dependencies
 import typer
 from typing_extensions import Annotated
+from rich import print
 
 # % GLOBAL
+
+# configuration files
 CONFIG_DIR = Path.home() / ".config" / "pyborg"
 PROFILES_FILE = CONFIG_DIR / "profiles.cfg"
+
+# display
+HEADER = ". [green]{}[/green]\n"
+PARAM = "  ├── {} : {}\n"
+LPARAM = "  └── {} : {}\n"
 
 # % DEFINE APP
 
@@ -34,9 +42,41 @@ app = typer.Typer(
 
 
 @app.command()
-def profiles():
+def profiles(
+    verbose: Annotated[
+        bool, typer.Option("--verbose", "-v", help="show profiles settings")
+    ] = False,
+):
     "Lists available borg profiles"
-    print("hello")
+    # -- check if config file exists
+    if not PROFILES_FILE.is_file():
+        msg = f"Cannot find the profile config file at '{PROFILES_FILE}'. "
+        msg += "You can initialize a config file using `pyborg init`."
+        print(msg)
+        raise typer.Abort()
+
+    # -- load config
+    config = configparser.ConfigParser(interpolation=None)
+    config.read(PROFILES_FILE)
+
+    # -- print title
+    msg = " 📄 List of Backup Profiles "
+    n = len(msg) + 1
+    print("╭" + "─" * n + "╮")
+    print(f"│[bold blue]{msg}[/bold blue]│")
+    print("╰" + "─" * n + "╯")
+
+    # -- print profiles
+    for s in config.sections():
+        info = HEADER.format(s)
+        if verbose:
+            out = []
+            for param, value in config[s].items():
+                out.append(PARAM.format(param, value))
+            out.pop()
+            out.append(LPARAM.format(param, value))
+            info += "".join(out)
+        print(info)
 
 
 @app.command()
@@ -65,8 +105,8 @@ def init():
     # - create default config
     config = configparser.ConfigParser(interpolation=None)
     config["example"] = {
-        "sudo": False,
         "description": "Save Important in ovh-sbg",
+        "sudo": False,
         "source": "/path/to/backup/source",
         "repository": "/path/to/borg/repo",
         "save_fmt": r"{now:%Y-%m-%d_%H:%M:%S}_{hostname}",
